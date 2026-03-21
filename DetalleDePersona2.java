2c18b6ed5ce80de46f0e363096e69273171ce74f
𝕱𝖊𝖗 <fermaldonado18609@gmail.com>
2026-03-20 23:05:36 -0300

    Diagnosticos
    Todos son diagnosticos desde la semana 1-2 hasta la fecha del 20

---
+++ DetalleDePersona2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Diagnostico;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Fer
+ */
+public class DetalleDePersona2 {
+    public static void main(String[] args) {
+        
+        
+        Scanner input = new Scanner (System.in);
+        String nombre="Mara Fernanda Maldonado";    
+        int edad= 16;
+        double altura= 1.78;
+        String pais= "Argentina"; 
+        char estadoCivil= 'S';
+    }
+    
+}
