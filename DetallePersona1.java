52442f79ee65693240a89cc855a3e55874309c41
𝕱𝖊𝖗 <fermaldonado18609@gmail.com>
2026-03-20 22:55:49 -0300

    Update DetallePersona1.java

--- DetallePersona1.java
+++ DetallePersona1.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Clase2;
+package Diagnostico;
 import java.util.Scanner;
 /**
  *
  * @author LABO B 10
  */
-public class DetallePersona1 {
+public class DetallePersona2 {
     public static void main(String[] args) {
         
         
